9c58f894694df04b8ef106641ad78748772a3295
Adriano Gil <adrianogil.san@gmail.com>
2019-01-07 12:58:40 -0400

    Added macro TEXT_EDITOR_APP

--- termux/export.sh
+++ termux/export.sh
@@ -1,1 +1,3 @@
 export TMP_DIR="$SDCARD/tmp/"
+export TEXT_EDITOR_APP="com.aor.droidedit.pro/com.aor.droidedit.DroidEditProActivity"
+# export TEXT_EDITOR_APP_PKG="com.aor.droidedit/.DroidEditFreeActivity"
