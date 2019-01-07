75cee7b0ffca7c8c8e4ff73eacd145f09830aede
Adriano Gil <adrianogil.san@gmail.com>
2019-01-07 15:18:49 -0400

    Added macro 3D_VIEWER_APP

--- termux/export.sh
+++ termux/export.sh
@@ -1,3 +1,4 @@
 export TMP_DIR="$SDCARD/tmp/"
 export TEXT_EDITOR_APP="com.aor.droidedit.pro/com.aor.droidedit.DroidEditProActivity"
 # export TEXT_EDITOR_APP_PKG="com.aor.droidedit/.DroidEditFreeActivity"
+export 3D_VIEWER_APP="com.dmitrybrant.modelviewer/.MainActivity"
