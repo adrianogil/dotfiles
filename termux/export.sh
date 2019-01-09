d83198e8d7b6699f106424d700c98c0ec0897f32
Adriano Gil <adrianogil.san@gmail.com>
2019-01-09 01:47:49 -0400

    Fixed macro name

--- termux/export.sh
+++ termux/export.sh
@@ -1,6 +1,6 @@
 export TMP_DIR="$SDCARD/tmp/"
 export TEXT_EDITOR_APP="com.aor.droidedit.pro/com.aor.droidedit.DroidEditProActivity"
 # export TEXT_EDITOR_APP_PKG="com.aor.droidedit/.DroidEditFreeActivity"
-export 3D_VIEWER_APP="com.dmitrybrant.modelviewer/.MainActivity"
+export MODEL3D_VIEWER_APP="com.dmitrybrant.modelviewer/.MainActivity"
 
 export BROWSER=termux-open-url
