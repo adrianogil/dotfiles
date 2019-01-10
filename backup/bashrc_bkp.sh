21faf749ecf6fe007e9437cb6b462a91f750b665
Adriano Gil <adrianogil.san@gmail.com>
2019-01-10 14:34:55 -0400

    Added function to load dots from backup

--- backup/bashrc_bkp.sh
+++ backup/bashrc_bkp.sh
@@ -6,6 +6,13 @@ function trybackup()
     [ -f "$source_file" ] && cp "$source_file" "$target_bkp_path"
 }
 
+function tryloadbackup()
+{
+    source_file=$1
+    target_bkp_path=$2
+    [ -f "$source_file" ] && cp "$target_bkp_path" "$source_file"
+}
+
 function dots-bkp()
 {
     bkp_folder=$PFILES_DIR/${CURRENT_MACHINE}_machine/bkp/
@@ -29,4 +36,20 @@ function dots-bkp()
     git rebase origin/master
     git push origin master
     cd $current_dir
+}
+
+function dots-update-from-bkp()
+{
+    bkp_folder=$PFILES_DIR/${CURRENT_MACHINE}_machine/bkp/
+
+    # Installation
+    tryloadbackup $HOME/.bash_install $bkp_folder/.bash_install
+
+    # Aliases
+    tryloadbackup $HOME/.bash_aliases $bkp_folder/.bash_aliases
+
+    # Another configs
+    tryloadbackup $HOME/.profile_config $bkp_folder/.profile_config
+    tryloadbackup $HOME/.bashrc_config $bkp_folder/.bashrc_config
+    tryloadbackup $HOME/.gitconfig $bkp_folder/.gitconfig
 }
