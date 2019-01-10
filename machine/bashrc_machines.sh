0c03bba2033caf96ebff9aacba90c846a74eb55f
Adriano Gil <adrianogil.san@gmail.com>
2019-01-10 16:06:25 -0400

    Added method to show machine name

--- machine/bashrc_machines.sh
+++ machine/bashrc_machines.sh
@@ -5,3 +5,8 @@ machine_define_file=$HOME/.machine
 [ -f "$machine_define_file" ] && machine_name=$(cat "$machine_define_file")
 
 export CURRENT_MACHINE=$machine_name
+
+function dot-machine-name()
+{
+    echo $CURRENT_MACHINE
+}
